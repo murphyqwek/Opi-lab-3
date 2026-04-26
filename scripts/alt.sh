d5b9a783962c95832f4b30e48639504995a92477
murphyqwek <qwekinaft@list.ru>
2026-04-26 17:41:13 +0300

    Final clear

--- scripts/alt.sh
+++ scripts/alt.sh
@@ -9,18 +9,13 @@ fi
 
 echo "Начинаем массовую замену имен..."
 
-# Читаем файл построчно
 while IFS=: read -r old_name new_name; do
     if [ -z "$old_name" ]; then continue; fi
 
     echo "Замена: $old_name -> $new_name"
 
-    # 1. Заменяем текст внутри всех .java файлов
-    # Используем \b для точного совпадения (чтобы не заменить Data в DataBean)
     find src/main/java -type f -name "*.java" -exec sed -i "s/\b$old_name\b/$new_name/g" {} +
 
-    # 2. Переименовываем файлы, если имя файла совпадает с именем класса
-    # Например: Point.java -> Location.java
     find src/main/java -type f -name "${old_name}.java" | while read -r file; do
         dir=$(dirname "$file")
         mv "$file" "$dir/${new_name}.java"
@@ -28,6 +23,5 @@ while IFS=: read -r old_name new_name; do
 
 done < "$MAPPING_FILE"
 
-# 3. После всех замен вызываем сборку, как требует задание
 echo "Запукаю сборку альтернативной версии..."
 ninja build
--- scripts/team.sh
+++ scripts/team.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-
-# Читаем параметры из файла
 PROJECT_NAME=$(grep "project_name =" params.ninja | cut -d'=' -f2 | tr -d ' ')
 DIST_DIR=$(grep "dist_dir =" params.ninja | cut -d'=' -f2 | tr -d ' ')
 
-# Получаем текущую ревизию
 CUR_REV=$(svn info --show-item revision)
 
-# Создаем временную папку для сбора артефактов
 mkdir -p team_builds
 
 echo "Начинаем сборку предыдущих ревизий..."
@@ -19,23 +15,19 @@ for i in 1 2 3; do
     echo "--- Обработка ревизии $REV ---"
     svn update -r $REV --quiet
     
-    # Обязательно чистим, чтобы Ninja пересобрал старый код
     ninja -t clean
     
     if ninja build; then
         echo "Сборка ревизии $REV прошла успешно."
-        # Копируем полученный jar во временную папку, переименовывая его
         cp "$DIST_DIR/$PROJECT_NAME.jar" "team_builds/rev_$REV.jar"
     else
         echo "Ошибка сборки ревизии $REV"
     fi
 done
 
-# Возвращаемся к исходной ревизии
 echo "Возврат к исходной ревизии $CUR_REV..."
 svn update -r $CUR_REV --quiet
 
-# Пакуем все собранные jar-файлы в один zip-архив
 if [ "$(ls -A team_builds)" ]; then
     zip -j "$DIST_DIR/team_builds.zip" team_builds/*.jar
     echo "Все версии упакованы в $DIST_DIR/team_builds.zip"
@@ -43,5 +35,4 @@ else
     echo "Нечего упаковывать."
 fi
 
-# Удаляем временную папку
 rm -rf team_builds
